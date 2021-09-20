91604d164e6caeef8a926ce82772c6332ce74d5a
reddevil013 <harshchaturvedi95@gmail.com>
2021-09-20 13:21:30 +0530

    addinf difficulty level and it's functionality

--- js/cell.js
+++ js/cell.js
@@ -1,62 +1,61 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid')
-  const flagsLeft = document.querySelector('#flags-left')
-  const result = document.querySelector('#result')
-  let width = 10
-  let bombAmount = 20
-  let flags = 0
-  let squares = []
-  let isGameOver = false
+const grid = document.querySelector('.grid')
+const flagsLeft = document.querySelector('#flags-left')
+const result = document.querySelector('#result')
+let width = 10
+let bombAmount = 10
+let flags = 0
+let squares = []
+let isGameOver = false
 
-  //create Board
-  function createBoard() {
-    flagsLeft.innerHTML = bombAmount
+function createBoard() {
+  flagsLeft.innerHTML = bombAmount
 
-    //get shuffled game array with random bombs
-    const bombsArray = Array(bombAmount).fill('bomb')
-    const emptyArray = Array(width*width - bombAmount).fill('valid')
-    const gameArray = emptyArray.concat(bombsArray)
-    const shuffledArray = gameArray.sort(() => Math.random() -0.5)
+  //get shuffled game array with random bombs
+  const bombsArray = Array(bombAmount).fill('bomb')
+  const emptyArray = Array(width*width - bombAmount).fill('valid')
+  const gameArray = emptyArray.concat(bombsArray)
+  const shuffledArray = gameArray.sort(() => Math.random() -0.5)
 
-    for(let i = 0; i < width*width; i++) {
-      const square = document.createElement('div')
-      square.setAttribute('id', i)
-      square.classList.add(shuffledArray[i])
-      grid.appendChild(square)
-      squares.push(square)
+  for(let i = 0; i < width*width; i++) {
+    const square = document.createElement('div')
+    square.setAttribute('id', i)
+    square.classList.add(shuffledArray[i])
+    grid.appendChild(square)
+    squares.push(square)
 
-      //normal click
-      square.addEventListener('click', function(e) {
-        click(square)
-      })
+    //normal click
+    square.addEventListener('click', function(e) {
+      click(square)
+    })
 
-      //cntrl and left click
-      square.oncontextmenu = function(e) {
-        e.preventDefault()
-        addFlag(square)
-      }
-    }
-
-    //add numbers
-    for (let i = 0; i < squares.length; i++) {
-      let total = 0
-      const isLeftEdge = (i % width === 0)
-      const isRightEdge = (i % width === width -1)
-
-      if (squares[i].classList.contains('valid')) {
-        if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total ++
-        if (i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total ++
-        if (i > 10 && squares[i -width].classList.contains('bomb')) total ++
-        if (i > 11 && !isLeftEdge && squares[i -1 -width].classList.contains('bomb')) total ++
-        if (i < 98 && !isRightEdge && squares[i +1].classList.contains('bomb')) total ++
-        if (i < 90 && !isLeftEdge && squares[i -1 +width].classList.contains('bomb')) total ++
-        if (i < 88 && !isRightEdge && squares[i +1 +width].classList.contains('bomb')) total ++
-        if (i < 89 && squares[i +width].classList.contains('bomb')) total ++
-        squares[i].setAttribute('data', total)
-      }
+    //cntrl and left click
+    square.oncontextmenu = function(e) {
+      e.preventDefault()
+      addFlag(square)
     }
   }
-  createBoard()
+
+  //add numbers
+  for (let i = 0; i < squares.length; i++) {
+    let total = 0
+    const isLeftEdge = (i % width === 0)
+    const isRightEdge = (i % width === width -1)
+
+    if (squares[i].classList.contains('valid')) {
+      if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total ++
+      if (i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total ++
+      if (i > 10 && squares[i -width].classList.contains('bomb')) total ++
+      if (i > 11 && !isLeftEdge && squares[i -1 -width].classList.contains('bomb')) total ++
+      if (i < 98 && !isRightEdge && squares[i +1].classList.contains('bomb')) total ++
+      if (i < 90 && !isLeftEdge && squares[i -1 +width].classList.contains('bomb')) total ++
+      if (i < 88 && !isRightEdge && squares[i +1 +width].classList.contains('bomb')) total ++
+      if (i < 89 && squares[i +width].classList.contains('bomb')) total ++
+      squares[i].setAttribute('data', total)
+    }
+  }
+}
+easy()
+
 
   //add Flag with right click
   function addFlag(square) {
@@ -188,4 +187,31 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   }
-})
+
+
+
+function easy() {
+  bombAmount = 10
+  while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+  createBoard()
+  result.innerHTML = 'Easy'
+
+}
+function medium() {
+  bombAmount = 15
+  while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+  createBoard()
+  result.innerHTML = 'Medium'
+}
+function hard() {
+  bombAmount = 20
+  while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+  createBoard()
+  result.innerHTML = 'Hard'
+}
